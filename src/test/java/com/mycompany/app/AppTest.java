d1ff5e86a8c740571b87064f6212a7981bc8685e
lcabrera07 <l.cabrera@outlook.com>
2021-02-28 14:14:29 -0800

    breaking it

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,8 +34,8 @@ public class AppTest
     {
         App.main(null);
         try {
-        	//assertEquals("TEST!" + System.getProperty("line.separator"), outContent.toString());
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        	assertEquals("TEST!" + System.getProperty("line.separator"), outContent.toString());
+            //assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
